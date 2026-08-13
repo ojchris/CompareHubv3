63ec149a64b5cac9c1c866cc9591fd980013d392
Chris Okpada <cokpada37@gmail.com>
2026-08-13 15:08:07 +0100

    Drush generte custom Compari theme, ignore temp fixes folder

---
+++ web/themes/custom/compari/js/compari.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * compari behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.compari = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
